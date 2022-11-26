444b2f0117caf6e02e05901260342bc214f6b8d8
Elie Ruvinga <ruvingaelie@gmail.com>
2022-11-26 09:23:36 +0100

    setup style for any components

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import Cards from '../../Components/Cards'
 
 const index = () => {
+
+    const freelance=[
+        {
+            label:"DevOPs",
+            title:"Administrator",
+            picture:"def.png",
+        },
+
+        {
+            label:"FullStack",
+            title:"PDG",
+            picture:"def.png",
+        },
+
+        {
+            label:"UXDesigner",
+            title:"Chef",
+            picture:"def.png",
+        }
+    ]
     return (
         <div>
            <p>
                 Freelances :
            </p>
+
+           <Cards datas= {freelance[0]}/>
         </div>
     );
 };
