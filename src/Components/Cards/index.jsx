9e2c52cc46a7400dc1c5bb1dc2137c9944bdade8
Elie Ruvinga <ruvingaelie@gmail.com>
2022-12-03 22:54:07 +0100

    setup a UI of profils availables

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import styled from "styled-components"
+import Colors from "../../style/colors/Color"
 
-const ContainerCard= styled.div`
+const ContainerCard = styled.div`
+    width:130px;
     display:flex;
     flex-direction: column;
+    justify-content:center;
     padding:15px;
+    border-radius: 25px;
+    background-color:${Colors.backgroundLight};
+    box-shadow:1px 1px 5px ${Colors.Navtext};
 `
-const CardLabel = styled.label`
-    color: #8186a0;
+const CardLabel = styled.span`
+    color: ${Colors.Navtext};
     font-size:0.8em;
+    ${(props) => props.nameValue && `color:${Colors.Questions}; `}
 `
 const CardImage = styled.img`
         width:80px;
         height:80px;
         border-radius:50%;
+        margin:15px 0px;
 `
 
 const index = (props) => {
     return (
         <ContainerCard>
-            <CardLabel> {props.datas.label}</CardLabel>
-            <CardImage src={props.datas.picture} alt='avatar'/>
-            <CardLabel>{props.datas.title}</CardLabel>   
+            <CardLabel >{props.datas.label}</CardLabel>
+            <CardImage src={props.datas.picture} alt='avatar' />
+            <CardLabel nameValue>{props.datas.title}</CardLabel>
         </ContainerCard>
-    );
+    ); {/*className='border' */ }
 };
 
 export default index;
