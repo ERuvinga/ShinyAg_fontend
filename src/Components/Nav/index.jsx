8a37498aebdbb2c6b4558f8dbd13b915455e1c4b
Elie Ruvinga <ruvingaelie@gmail.com>
2023-01-05 16:43:58 +0100

    animate a boutonn dark and Light mode

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom'
 import styled from "styled-components"
 import Color from '../../style/colors/Color';
@@ -14,43 +14,64 @@ const NavLabel = styled.nav`
 const NavBarStyle = styled.div`
     display:flex;
     justify-content: space-around;
+    align-items: center;
     width:35%;
 `
 const LinkStyle = styled(Link)`
     color: ${Color.Navtext};
     text-decoration: none;
+    text-align: center;
     padding: 5px 0px;
     font-size: .9em;
 
     ${(props) => props.isfulllink && `background-color : ${Color.primary}; color:white; padding: 5px 20px; border-radius:30px; `
     }
 `
-const ChangeModeBtn = styled.span`
-    text-decoration: none;
-    color: ${Color.Headtext};
-    padding: 5px 0px;
+
+const DarkLightStyled = styled.span`
+    width:60px;
+    height: 25px;
+    border: 1px solid ${Color.Navtext};
+    border-radius: 20px;
+
+    color: ${Color.Navtext};
     font-size: .9em;
     cursor: pointer;
 `
+const Circle = styled.img`
+    padding: 2px;
+    width: 22px;
+    height: 22px;
+    border-radius: 20px;
+    background-color: ${Color.Navtext};
+    opacity: .5;
+    transition: all 1s;
 
+    &: hover{
+        opacity: 1;
+        transform: scale(2) rotate(360deg);
+    }
+
+    ${(props) => props.mode && `opacity : 0.5; transform: rotate(0deg)`
+    }
+`
 
 const NavBar = () => {
     const { theme, toogleTheme } = useContext(ThemeContext);
-    const [Mode, setMode] = useState(theme);
 
     const HandletoogleMode = () => {
         toogleTheme();
-        setMode(theme);
     }
+
 
     return (
         <NavLabel>
             <img src={theme === 'light' ? '/dark-logo.png' : '/light-logo.png'} alt='logo' className='logo' />
-            <NavBarStyle>
-                <ChangeModeBtn onClick={HandletoogleMode}>{Mode}</ChangeModeBtn>
+            <NavBarStyle className=''>
+                <DarkLightStyled onClick={HandletoogleMode}><Circle src={theme === 'light' ? 'light.png' : '/dark.png'} mode={theme === 'light' ? true : false} className='' /> </DarkLightStyled>
                 <LinkStyle exact="true" to="/" className='NavLink'> Home </LinkStyle>
                 <LinkStyle exact="true" to="/Freelances" className='NavLink'> Profils</LinkStyle>
-                <LinkStyle isfulllink='true' exact="true" to="/Questions" className='NavLink'> Faire le test</LinkStyle>
+                <LinkStyle isfulllink='true' exact="true" to="/Questions" className='NavLink '> Faire le test</LinkStyle>
             </NavBarStyle>
         </NavLabel>
     );
